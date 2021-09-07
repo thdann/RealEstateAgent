cc4b2bd1f1dd7f8fb77087a6f8b65fefdb2d1c1b
catarola <62558054+catarola@users.noreply.github.com>
2021-09-07 15:20:59 +0200

    Update TownHouse.cs
    town

--- RealEstateAgent/TownHouse.cs
+++ RealEstateAgent/TownHouse.cs
@@ -7,5 +7,26 @@ namespace RealEstateAgent
 {
     public class TownHouse : Residential
     {
+        private int numberOfFloors;
+
+        public TownHouse (int id, Address address, int numberOfRooms, int numberOfFloors) 
+            : base (id, address, numberOfRooms)
+        {
+            this.numberOfFloors = numberOfFloors;
+
+        }
+
+        public override int NumberOfRooms { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); }
+        public override int Id { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); }
+        public override Address Address { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); }
+        public override LegalForm LegalForm { 
+            get => throw new NotImplementedException(); s
+            set => throw new NotImplementedException(); }
     }
 }
